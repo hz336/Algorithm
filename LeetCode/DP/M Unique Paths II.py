c284a79bbb6dcbea21a659b263dd2d4e7e4144b8
Haoran Zhao <hz336@cornell.edu>
2019-06-26 22:53:27 -0400

    test

---
+++ LeetCode/DP/M Unique Paths II.py
@@ -0,0 +1,55 @@
+"""
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish'
+in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+"""
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid is None or len(obstacleGrid) == 0 or len(obstacleGrid[0]) == 0:
+            return 0
+
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        f = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    continue
+
+                if i == 0 and j == 0:
+                    f[i][j] = 1
+                elif i == 0 and j > 0:
+                    f[i][j] = f[i][j - 1]
+                elif i > 0 and j == 0:
+                    f[i][j] = f[i - 1][j]
+                else:
+                    f[i][j] = f[i - 1][j] + f[i][j - 1]
+
+        return f[m - 1][n - 1]
