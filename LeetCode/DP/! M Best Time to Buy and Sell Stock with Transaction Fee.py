44cf6c349cead8535c37119be1c6fa1943f6f350
hz336 <hz336@cornell.edu>
2019-07-09 17:39:42 -0400

    Update ! M Best Time to Buy and Sell Stock with Transaction Fee.py

--- LeetCode/DP/! M Best Time to Buy and Sell Stock with Transaction Fee.py
+++ LeetCode/DP/! M Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -23,6 +23,14 @@ buy[i] = max(buy[i - 1], sell[i - 1] - price[i - 1])
 """
 
 """
+考虑每一天同时维护两种状态：拥有股票(buy)状态和已经售出股票(sell)状态。用buy和sell分别保留这两种状态到目前为止所拥有的最大利润。 
+对于sell，用前一天buy状态转移，比较卖出持有股是否能得到更多的利润，即sell = max(sell , buy + price - fee)， 
+而对于sell, 我们考虑是否买新的股票更能赚钱(换言之，更优惠），buy = max(buy, sell-price)。
+初始化我们要把sell设为0表示最初是sell状态且没有profit，把buy设为负无穷因为最初不存在该状态，我们不希望从这个状态进行转移
+因为我们保存的都是最优状态，所以在买卖股票时候取max能保证最优性不变
+"""
+
+"""
 Time Complexity: O(n)
 Space Complexity: O(n)
 """
