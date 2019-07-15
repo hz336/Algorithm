f690e36b02761e4efd91bbee88070c25334e494d
Haoran Zhao <hz336@cornell.edu>
2019-07-15 12:29:01 -0400

    .

--- LeetCode/DP/H Russian Doll Envelopes.py
+++ LeetCode/DP/H Russian Doll Envelopes.py
@@ -34,3 +34,73 @@ class Solution:
 
         return max(f)
 
+
+import bisect
+
+"""
+Dynamic Programming + Binary Search 
+Time Complexity: O(nlogn) 
+"""
+class Solution_v1:
+    """
+    @param: envelopes: a number of envelopes with widths and heights
+    @return: the maximum number of envelopes
+    """
+
+    def maxEnvelopes(self, envelopes):
+        # write your code here
+        height = [a[1] for a in sorted(envelopes, key=lambda x: (x[0], -x[1]))]
+
+        len_height = len(height)
+        min_last = [math.inf] * (len_height + 1)
+        min_last[0] = -math.inf
+
+        for i in range(len_height):
+            # find the first number in minLast >= nums[i]
+            index = self.binary_search(min_last, height[i])
+            min_last[index] = height[i]
+
+        for i in range(len_height, 0, -1):
+            if min_last[i] != math.inf:
+                return i
+
+        return 0
+
+    def binary_search(self, min_last, target):
+        start, end = 0, len(min_last) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if min_last[mid] < target:
+                start = mid
+            else:
+                end = mid
+
+        if min_last[start] >= target:
+            return start
+        else:
+            return end
+
+
+"""
+Dynamic Programming + Binary Search 
+Time Complexity: O(nlogn) 
+"""
+class Solution:
+    """
+    @param: envelopes: a number of envelopes with widths and heights
+    @return: the maximum number of envelopes
+    """
+
+    def maxEnvelopes(self, envelopes):
+        # write your code here
+        height = [a[1] for a in sorted(envelopes, key=lambda x: (x[0], -x[1]))]
+        min_last = [0] * len(height)
+
+        start, end = 0, 0
+        for h in height:
+            i = bisect.bisect_left(min_last, h, start, end)
+            min_last[i] = h
+            if i == end:
+                end += 1
+
+        return end
