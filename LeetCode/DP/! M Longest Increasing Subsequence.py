a089f1acdba78f07944b6739bc90e06e9da42912
Haoran Zhao <hz336@cornell.edu>
2019-07-15 12:24:04 -0400

    .

--- LeetCode/DP/! M Longest Increasing Subsequence.py
+++ LeetCode/DP/! M Longest Increasing Subsequence.py
@@ -32,3 +32,59 @@ class Solution:
         return max(f)
 
 
+"""
+Binary Search 
+Just remember the algorithm 
+Time Complexity: O(nlogn)
+"""
+
+"""
+For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+
+len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
+len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
+len = 3   :      [4, 5, 6]            => tails[2] = 6
+We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
+
+Each time we only do one of the two:
+
+(1) if x is larger than all tails, append it, increase the size by 1
+(2) if tails[i-1] < x <= tails[i], update tails[i]
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if nums is None or len(nums) == 0:
+            return 0
+
+        n = len(nums)
+        min_last = [float('inf')] * n
+
+        for i in range(n):
+            # find the first number in minLast >= nums[i]
+            index = self.binary_search(min_last, nums[i])
+            min_last[index] = nums[i]
+
+        for i in range(n - 1, -1, -1):
+            if min_last[i] != float('inf'):
+                return i + 1
+
+        return 0
+
+    def binary_search(self, min_last, target):
+        start, end = 0, len(min_last) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if min_last[mid] < target:
+                start = mid
+            else:
+                end = mid
+
+        if min_last[start] >= target:
+            return start
+        else:
+            return end
+
+
+
